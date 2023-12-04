3eb9aee0ffc9dc8b075e9470c075fd3548f70bf1
TheConqueror327 <mark.erdohelyi@gmail.com>
2023-12-04 17:29:49 +0100

    Faster intersection function

--- point_location_test.py
+++ point_location_test.py
@@ -1,14 +1,21 @@
-def intersects(p1: list, p2: list, p3: list) -> str:
-    if p3[0] / p3[1] > p1[0] / p1[1]:
+def intersection(p1: list, p2: list, p3: list) -> str:
+    m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+    b = p1[1] - m * p1[0]
+    if p3[1] > m * p3[0] + b:
         return 'LEFT'
-    elif p3[0] / p3[1] < p1[0] / p1[1]:
+    if p3[1] < m * p3[0] + b:
         return 'RIGHT'
-    else:
-        return 'TOUCH'
-    
-#print(intersects([-1, -1], [1, 1], [2, 1]))
-tests = []
-for i in range(3):
-    tests.append(list[map(int, int(input().split()))])
+    return 'TOUCH'
 
-print(tests.pop(0))
+def faster_intersection(p1: list, p2: list, p3: list) -> str:
+    if (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0]) > 0:
+        return 'LEFT'
+    elif (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0]) < 0:
+        return 'RIGHT'
+    return 'TOUCH'
+
+t = int(input())
+
+for i in range(t):
+    current_test = list(map(int, input().split()))
+    print(faster_intersection(current_test[:2], current_test[2:4], current_test[4:]))
