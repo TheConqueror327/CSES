7e382abdab6da51b8961ed3e52cfc663998e8b08
TheConqueror327 <mark.erdohelyi@gmail.com>
2023-12-06 17:28:43 +0100

    Modify minimizing_coins.py

--- minimizing_coins.py
+++ minimizing_coins.py
@@ -2,16 +2,18 @@ n, inp = map(int, input().split())
 coins = list(map(int, input().split()))
 
 def minimizing_coins(n: int, coins: list) -> int:
-    d = {n: 0}
+    d = [-1] * (n + 1)
+    d[n] = 0
     f = [n]
     while len(f) > 0:
         k = f.pop()
-        v = d.get(k)
+        v = d[k]
         for i in coins:
-            e = d.get(k - i, -1)
-            if (v + 1 < e or e == -1) and k - i >= 0:
-                d[k - i] = v + 1
-                f.append(k - i)
-    return d.get(0, -1)
+            if k - i >= 0:
+                e = d[k - i]
+                if (v + 1 < e or e == -1):
+                    d[k - i] = v + 1
+                    f.append(k - i)
+    return d[0]
 
 print(minimizing_coins(inp, coins))
