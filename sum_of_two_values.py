702b4ad5190e09048752c19c60b7cad60b11fcb4
TheConqueror327 <mark.erdohelyi@gmail.com>
2023-12-06 19:57:06 +0100

    Sum of Two Values

--- sum_of_two_values.py
+++ sum_of_two_values.py
@@ -3,9 +3,9 @@ lis = list(map(int, input().split()))
 
 def sum_of_two_values(li: list, x: int) -> int:
     for i, v in enumerate(li):
-        for o, w in enumerate(li[i:]):
+        for o, w in enumerate(li[i+1:]):
             if v + w == x:
-                return f'{o + 1} {i + 1}'
+                return f'{i + 1} {o + i + 2}'
     return 'IMPOSSIBLE'
 
 print(sum_of_two_values(lis, xx))
